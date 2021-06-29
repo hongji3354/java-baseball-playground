54ff995750c7c734249fc8862231faf13745f164
JiHun Hong <hongji3354@gmail.com>
2021-06-29 15:15:34 +0900

    feat : String 클래스 학습 테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,38 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void split() {
+        String[] actual = "1,2".split(",");
+        assertThat(actual).contains("1","2");
+        assertThat(actual).containsExactly("1","2");
+    }
+
+    @Test
+    void substring() {
+        String actual = "(1,2)".substring(1,4);
+        assertThat(actual).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("String charAt Index Test")
+    void charAt() {
+        String actual = "abc";
+        assertThat(actual.charAt(2)).isEqualTo('c');
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+                .isThrownBy(() -> {
+                    actual.charAt(3);
+                }).withMessageMatching("String index out of range: 3");
     }
 }
