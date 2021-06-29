67738aaa72b35c36976ea494aa9e62b6f4880a33
JiHun Hong <hongji3354@gmail.com>
2021-06-29 15:16:01 +0900

    feat : Set Collection 학습 테스트

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,45 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void size() {
+        assertEquals(3, numbers.size());
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    void contains(int number) {
+        assertTrue(numbers.contains(number));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void containsInputDifferent(int number, boolean expected) {
+        assertEquals(numbers.contains(number), expected);
+    }
+}
