405d7a408ae6f57ac06646c0e1e49a87c5735305
JiHun Hong <hongji3354@gmail.com>
2021-06-29 16:08:04 +0900

    refactor : 문자열 계산기의 계산 로직 Enum으로 분리

--- src/test/java/study/CalculatorTest.java
+++ src/test/java/study/CalculatorTest.java
@@ -1,7 +1,12 @@
 package study;
 
+import org.assertj.core.internal.bytebuddy.implementation.bytecode.Multiplication;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.function.BinaryOperator;
+import java.util.function.IntBinaryOperator;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -15,29 +20,39 @@ class CalculatorTest {
 
         for (int i = 0; i < values.length; i++) {
             if (i % 2 != 0 ) {
+                Expression expression = Expression.of(values[i]);
                 int value = Integer.parseInt(values[i+1]);
-                if (values[i].equals("+")) {
-                    sum += value;
-                    continue;
-                }
-
-                if (values[i].equals("*")) {
-                    sum *= value;
-                    continue;
-                }
-
-                if (values[i].equals("/")) {
-                    sum /= value;
-                    continue;
-                }
-
-                if (values[i].equals("-")) {
-                    sum -= value;
-                }
+                sum = expression.calculation(sum, value);
             }
         }
 
         assertEquals(sum, expected);
     }
 
+}
+
+enum Expression {
+    ADD("+", (a,b) -> a + b),
+    MINUS("-", (a,b) -> a - b),
+    MULTIPLICATION("*", (a,b) -> a * b),
+    DIVISION("/", (a,b) -> a / b);
+
+    private String expression;
+    private IntBinaryOperator operator;
+
+    Expression(String expression, IntBinaryOperator operator) {
+        this.expression = expression;
+        this.operator = operator;
+    }
+
+    static Expression of(String expression) {
+        return Stream.of(values())
+                    .filter(v -> v.expression.equals(expression))
+                    .findFirst()
+                    .orElseThrow(() -> new IllegalArgumentException(String.format("%s는 사칙연산에 해당하지 않는 표현식입니다.", expression)));
+    }
+
+    public int calculation(int left, int right) {
+        return this.operator.applyAsInt(left, right);
+    }
 }
